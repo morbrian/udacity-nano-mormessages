91775941e1ddbc98672aa2dd1741d0f273fa7295
Brian Moriarty <morbrian@me.com>
2016-02-07 11:25:12 -0800

    issue-#28 setting aside the goal of using UITextView for sending messages in favor of a simple UITextField, planning to revisit this, but it was difficult to implement correctly and schedule constraints won't allow for more time right now.

---
+++ MorMessages/DetailsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  DetailsViewController.swift
+//  MorMessages
+//
+//  Created by Brian Moriarty on 2/6/16.
+//  Copyright © 2016 Brian Moriarty. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DetailsViewController: UIViewController {
+        
+    var manager: MorMessagesManager!
+    var forum: Forum!
+    
+    var lastTextViewHeight: CGFloat = 0.0
+    
+    
+    // MARK: ViewController Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
